5e1baf1e866e92bc38bcf4a8ed88e0e5db695e6c
Jordan Fasso <jordan.fasso@gmail.com>
2019-04-24 16:10:44 +1000

    Updated files

---
+++ README.md
@@ -0,0 +1,37 @@
+# Terminal App: Political Profile
+
+## Contributors
+
+Jordan Fasso & Nathan Pallett
+
+____________
+
+## Link
+
+[Political Profile App](https://github.com/joryaboya/Terminal-App-Political-Quiz "Terminal App")
+
+__________
+
+## Purpose
+
+With the upcoming election it 
+
+____
+## Functionality 
+
+____
+
+# Design Plan
+
+### Notebook
+
+### Trello Use
+
+### General Progress Report
+
+### Coding Progression
+
+### Challenges Faced
+
+### Stretch Goals
+
---
+++ party_responses.rb
@@ -0,0 +1,67 @@
+party_values = [
+    liberal = {
+        environmentalist: -4,
+        capitalist: 4,
+        humanitarian: 0,
+        authoritarian: 0, 
+        abortion: 1,
+        asylum: -2,
+        death_penalty: -1,
+        director_quotas: -2,
+        drug_testing: 2,
+        euthanasia: 2,
+        monarchy: -2,
+        nuclear_energy: 2,
+        parental_leave: 2,
+        same_sex_marriage: 2,
+        ssc: -2,
+        vaccines: 2
+    },
+    labor = {
+        environmentalist: 2,
+        capitalist: 0,
+        humanitarian: 2,
+        authoritarian: -4,
+        abortion: 2,
+        asylum: 1,
+        death_penalty: -1,
+        director_quotas: 2,
+        drug_testing: 2,
+        euthanasia: 2,
+        monarchy: 2,
+        nuclear_energy: -2,
+        parental_leave: -2,
+        same_sex_marriage: 2,
+        ssc: 2,
+        vaccines: 2
+    },
+    greens = {
+        environmentalist: 4,
+        capitalist: -4,
+        humanitarian: 4,
+        authoritarian: -4,
+        abortion: 2,
+        asylum: 2,
+        death_penalty: -2,
+        director_quotas: 2,
+        drug_testing: 2,
+        euthanasia: 2,
+        monarchy: 2,
+        nuclear_energy: -2,
+        parental_leave: 2,
+        same_sex_marriage: 1,
+        ssc: 2,
+        vaccines: 2
+    } ]
+    @asylum = 0 
+        @death_penalty = 0
+        @director_quotas = 0
+        @drug_testing = 0
+        @euthanasia = 0
+        @grapes = 0
+        @monarchy = 0
+        @nuclear_energy = 0
+        @parental_leave = 0
+        @same_sex_marriage = 0
+        @ssc = 0
+        @vaccines = 0
